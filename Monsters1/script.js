d7dcb7294b648aa245a082869f074572d9749817
johnsonconsulting2 <johnsonconsulting2@gmail.com>
2014-06-03 14:26:09 -0400

    Added Packages constructor function

---
+++ Monsters1/script.js
@@ -0,0 +1,29 @@
+﻿//create my object
+var myApp = {};
+
+//this is the array to hold my monsters
+myApp.Monsters = [];
+
+//create the monsters with a Constructor
+myApp.Monster = function (name, description, picture) {
+    this.name = name;
+    this.description = description;
+    this.picture = picture;
+};
+
+//write tables of monsters out to the page
+myApp.WriteTable = function () { };
+
+//CRUD functions
+
+//create monster
+myApp.CreateMonster = function () { };
+
+//show updated monster
+myApp.ShowUpdateMonster = function () { };
+
+//save updated monster
+myApp.SaveUpdateMonster = function () { };
+
+//delete monster
+myApp.DeleteMonster = function () { };
